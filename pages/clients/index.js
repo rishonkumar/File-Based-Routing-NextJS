c4636865327aeb32522fd6ed88acc7140c43f9c2
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-09-28 22:30:47 +0530

    Dynamic rendering

---
+++ pages/clients/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ClientsPages() {
+  return (
+    <div>
+      <h1>ClientsPages</h1>
+    </div>
+  );
+}
+
+export default ClientsPages;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>Hello PortFoliooo!!!</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
